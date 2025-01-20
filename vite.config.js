d39582d3182bf78e3b6a51f43f1172d3cd930ce9
Nikhilsingh14k <nikhilsingh14k@gmail.com>
2025-01-20 19:36:53 +0530

    done -2

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Bind to 0.0.0.0
-    port: 3000, // Use Render's PORT environment variable
-  },
-  preview: {
-    host: '0.0.0.0', // Bind the preview server to 0.0.0.0
-    port: 3000,
-  },
-});
+})
